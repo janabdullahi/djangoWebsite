e316daa35042105b982053da341494f73f0e48f0
abdullahi <jan.ab1517@gmail.com>
2023-09-30 10:26:07 +0100

    change the app name in setting correctly, add template for the base app, add url for the app project and include it to the project url

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'BaseConfig',
+    'base',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
